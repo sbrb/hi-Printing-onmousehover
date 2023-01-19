e2f21913d42094782c8c9c57c569ba6c0ec682f2
sbrb <info.sbriju@gmail.com>
2023-01-19 14:15:25 +0530

    s

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hi-Printing-On-Mouse-Hover
+
+Using HTML, CSS, JavaScript.
+<img src='img.png' width='100%' height='100%'>
---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  // Solution2:-
+  // document.getElementById('myDiv').append('H! ');
+  // Solution1:-
+  document.getElementById('myDiv').innerHTML += 'H! ';
+}
